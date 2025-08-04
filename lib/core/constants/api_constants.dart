7b789ecfe50b3cf4f2d73f1c7c581ca91eda4cd3
requinpham <kyanhpham0403@gmail.com>
2025-08-05 02:21:12 +0700

    Basic Register

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,13 +1,16 @@
 class ApiConstants {
   // Base URL có thể đổi 1 nơi duy nhất ở đây
-  static const String ipLocal = '192.168.1.28';
+  static const String ipLocal = '192.168.1.55';
   static const String baseUrl = 'http://' + ipLocal + ':9999/api';
 
   // Ví dụ: các endpoint cụ thể
   static const String login = baseUrl + '/users/login';
-  static const String register = baseUrl + '/auth/register';
+  static const String register = baseUrl + '/users/register';
   static const String getTests = baseUrl + '/test/';
-  static const String getProfile = baseUrl + '/users/profile';
+  static const String getProfile = baseUrl + '/users/profile/';
+  static const String sendOtp = baseUrl + '/users/send-otp-register';
+  static const String verifyOtp = baseUrl + '/users/verify-otp-register';
+  static const String checkEmailExists = baseUrl + '/users/check-email';
 
   //chat api
   static const String getChatSessionByUserId = baseUrl + '/chat/session';
