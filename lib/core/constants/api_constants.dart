ba488338d5d0882497606af1f80d1f3beeee9d14
P52 <dangminhquan9320@gmail.com>
2025-09-18 10:17:12 +0700

    ok

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // Base URL có thể đổi 1 nơi duy nhất ở đây
-  static const String ipLocal = '192.168.1.6';
+  static const String ipLocal = '192.168.3.244  ';
   static const String baseUrl = 'http://' + ipLocal + ':9999/api';
   // Ví dụ: các endpoint cụ thể
   static const String login = baseUrl + '/users/login';
